0c4811910f41cfbf570af6ef951f38d1ee72a8b7
Baeldung CI User <baeldungci@baeldung.com>
2025-09-29 13:32:51 +0000

    Merge pull request #10 from Baeldung/rozagerardo/module1_COURSE-260_varaible-types--lead-review
    [COURSE-260] Using println instead of print

---
+++ variables-types-end/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int age;
+        age = 20;
+
+        System.out.println(age);
+
+        int steps = 20;
+        System.out.println(steps);
+
+        steps = 30;
+        System.out.println(steps);
+
+        int playerScore = 100;
+        System.out.println("The player’s score is: ");
+        System.out.println(playerScore);
+
+        double salary = 1000.5;
+        System.out.println("The worker's salary is: ");
+        System.out.println(salary);
+
+        float temperature = 25.6f;
+        System.out.println("Outside temperature is: ");
+        System.out.println(temperature);
+
+        boolean closed = false;
+        System.out.println("Is the task closed? ");
+        System.out.println(closed);
+
+        String greeting = "Hello, world!";
+        System.out.println(greeting);
+
+        String s1 = "Hello";
+        System.out.println(s1);
+    }
+}
